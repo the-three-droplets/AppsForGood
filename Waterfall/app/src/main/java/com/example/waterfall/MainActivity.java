fe4526348394e14b2c0711217402131f1955e276
EricLi <eric2927.li@gmail.com>
2019-04-03 10:05:31 -0400

    Began working on notifications

--- Waterfall/app/src/main/java/com/example/waterfall/MainActivity.java
+++ Waterfall/app/src/main/java/com/example/waterfall/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.waterfall;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -14,11 +17,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "Bharath";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.navbarBottom);
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -68,6 +73,24 @@ public class MainActivity extends AppCompatActivity {
 
         TextView percentageDrank = (TextView) findViewById(R.id.text_percent_drank);
         TextView fractionDrank = (TextView) findViewById(R.id.text_fraction_drank);
+
+    }
+
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
